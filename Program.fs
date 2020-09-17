052f5427d0b39eb9c1284dc70a98c15a74f5d16d
akitsu-sanae <akitsu.sanae@gmail.com>
2020-09-17 14:26:54 +0900

    composite key

--- Program.fs
+++ Program.fs
@@ -12,7 +12,7 @@ type Struct =
   | Scalar of string
   | Record of (Key * Struct) list
 
-type Relation = {Src: string; Dist: string * string}
+type Relation = {Src: string list; Dist: string * string}
 
 [<Struct>]
 type Entity = {
@@ -91,15 +91,20 @@ module Parse =
 
   let relationFromYamlNode (node: YamlNode): Result<Relation> =
     let relationYaml : YamlScalarNode = downcast node
-    let regex = Regex("^\\((?<src>.+?)\\) -> (?<distEntity>.+?)\\((?<distField>.+?)\\)$")
+    let regex = Regex("^\\((?<src>.+?)(, (?<srcTail>.+?))*\\) -> (?<distEntity>.+?)\\((?<distField>.+?)\\)$")
     let m = regex.Match(relationYaml.Value)
-    Ok {
-      Data = {
-        Src =  m.Groups.["src"].Value
-        Dist = m.Groups.["distEntity"].Value, m.Groups.["distField"].Value
+    if m.Success then
+      let srcHead = m.Groups.["src"].Value
+      let srcTails = Seq.map (fun (capture:Capture) -> capture.Value) m.Groups.["srcTail"].Captures |> Seq.toList
+      Ok {
+        Data = {
+          Src =  srcHead :: srcTails
+          Dist = m.Groups.["distEntity"].Value, m.Groups.["distField"].Value
+        }
+        Warnings = []
       }
-      Warnings = []
-    }
+    else
+      parseError node (String.Format("`{0}` is not acceptable as relation", node.ToString()))
 
   let entityFromYamlNode (name: string) (node: YamlNode): Result<Entity> =
     let node : YamlMappingNode = downcast node
@@ -160,7 +165,7 @@ module Print =
     Struct: (Key * Struct) list
   }
   type Edge = {
-    Src: string * string
+    Src: (string * string) list
     Dist: string * string
   }
   type Graphviz = {
@@ -183,7 +188,7 @@ module Print =
           List.fold
             (fun acc (relation:Relation) ->
               {
-                Src = entity.Name, relation.Src
+                Src = List.map (fun src -> entity.Name, src) relation.Src
                 Dist = relation.Dist
               } :: acc
             )
@@ -210,10 +215,33 @@ module Print =
   let nodeToString (node: Node): string =
     String.Format("""  {0} [label="<{0}>{0} | {1}\l"]""", makeValidLabel node.Name, recordToString node.Struct)
 
+  let mutable edgeInterCount = 0
+
+  let edgeToString edge =
+    if edge.Src.Length = 1 then
+      let src = edge.Src.[0]
+      [String.Format("  {0}:{1} -> {2}:{3}", makeValidLabel <| fst src, makeValidLabel <| snd src, makeValidLabel <| fst edge.Dist, makeValidLabel <| snd edge.Dist)], []
+    else
+      let intermediate = String.Format("__intermediate{0}__", edgeInterCount)
+      edgeInterCount <- edgeInterCount + 1
+      let edges =
+        (String.Format("  {0} -> {1}:{2}", intermediate, makeValidLabel <| fst edge.Dist, makeValidLabel <| snd edge.Dist)) ::
+        List.map
+          (fun src ->
+            String.Format("  {0}:{1} -> {2} [dir=none]", makeValidLabel <| fst src, makeValidLabel <| snd src, intermediate)
+          )
+          edge.Src
+      let interNodes = String.Format("  {0} [shape=point, width=0.01, height=0.01]", intermediate)
+      edges, [interNodes]
+
   let graphvizToString (graphviz: Graphviz): string =
     let nodes = List.map nodeToString graphviz.Nodes |> String.concat "\n"
-    let edgeToString = fun edge -> String.Format("  {0}:{1} -> {2}:{3}", makeValidLabel <| fst edge.Src, makeValidLabel <| snd edge.Src, makeValidLabel <| fst edge.Dist, makeValidLabel <| snd edge.Dist)
-    let edges = List.map edgeToString graphviz.Edges |> String.concat "\n"
+    let edges, interNodes =
+      List.fold
+        (fun (accEdges, accInterNodes) edge ->
+          let (edges, interNodes) = edgeToString edge
+          (edges @ accEdges, interNodes @ accInterNodes)
+        ) ([], []) graphviz.Edges
     String.Format("""
 digraph Schema {{
   rankdir=LR
@@ -228,10 +256,11 @@ digraph Schema {{
   ]
 
 {0}
-
 {1}
+
+{2}
 }}
-""", nodes, edges)
+""", nodes, interNodes |> String.concat "\n", edges |> String.concat "\n")
 
   let schemaToFile (filename: string) (schema: Schema) =
     let content = schemaToGraphviz schema |> graphvizToString
