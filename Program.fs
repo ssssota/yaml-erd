58b6afa132009828284f39b9a07429ccf4e1345d
akitsu-sanae <akitsu.sanae@gmail.com>
2020-09-17 18:17:26 +0900

    fix printing error message

--- Program.fs
+++ Program.fs
@@ -31,7 +31,7 @@ type Warning = {
 }
 
 let warningToConsoleString w =
-  String.Format("\033[33m warning[{0}:{1}-{2}:{3}]: \033[0m {4}", w.StartLine, w.StartColumn, w.EndLine, w.EndColumn, w.Message)
+  String.Format("\x1b[33m warning[{0}:{1}-{2}:{3}]: \x1b[0m {4}", w.StartLine, w.StartColumn, w.EndLine, w.EndColumn, w.Message)
 
 type ErrorValue = {
   StartLine: int
@@ -42,7 +42,7 @@ type ErrorValue = {
 }
 
 let errorToConsoleString e =
-  String.Format("\033[31m error[{0}:{1}-{2}:{3}]: \033[0m {4}", e.StartLine, e.StartColumn, e.EndLine, e.EndColumn, e.Message)
+  String.Format("\x1b[31m error[{0}:{1}-{2}:{3}]: \x1b[0m {4}", e.StartLine, e.StartColumn, e.EndLine, e.EndColumn, e.Message)
 
 type OkValue<'a> = {Data: 'a; Warnings: Warning list}
 type Result<'a> = Result<OkValue<'a>, ErrorValue list>
