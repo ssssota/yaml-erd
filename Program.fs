85ee92167ce61bf16b545cb72f67542b0603a1de
akitsu-sanae <akitsu.sanae@gmail.com>
2020-10-21 16:32:07 +0900

    update

--- Program.fs
+++ Program.fs
@@ -1,8 +1,33 @@
 ﻿open Util
+open Argu
 
-match Parse.schemaFromFile @"./sample_.yaml" |> Result.bind Validation.validate |> Result.bind PostProc.postProc with
-| Ok { Data = schema; Warnings = warnings } ->
-    List.iter (fun warning -> Printf.eprintfn "%s" <| warningToConsoleString warning) warnings;
-    Print.schemaToFile @"./output.dot" schema
-| Error errs ->
-  List.iter (fun err -> Printf.eprintfn "%s" <| errorToConsoleString err) errs
+type Arguments =
+  | [<MainCommand; ExactlyOnce>]Input of filename:string
+  | [<AltCommandLine("-o")>]Output of filename:string
+
+  interface IArgParserTemplate with
+    member s.Usage =
+        match s with
+        | Input _ -> "input yaml filename"
+        | Output _ -> "output dot filename"
+
+[<EntryPoint>]
+let main args =
+  let parser = ArgumentParser.Create<Arguments>(programName = "yaml-erd")
+  try
+    let input, output = List.fold (fun (input, output) -> function
+      | Input path -> path, output
+      | Output path -> input, path) ("", "./output.dot") <| (parser.Parse args).GetAllResults()
+
+    match Parse.schemaFromFile input |> Result.bind Validation.validate |> Result.bind PostProc.postProc with
+    | Ok { Data = schema; Warnings = warnings } ->
+      List.iter (fun warning -> Printf.eprintfn "%s" <| warningToConsoleString warning) warnings;
+      Print.schemaToFile output schema
+      0
+    | Error errs ->
+      List.iter (fun err -> Printf.eprintfn "%s" <| errorToConsoleString err) errs
+      -1
+  with
+  | _ ->
+    eprintf "%s" <| parser.PrintUsage()
+    -1
